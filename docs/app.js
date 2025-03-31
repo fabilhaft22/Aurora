6f6315faa77da28b23ea35336903f07bbb535375
fabi <113532505+fabilhaft22@users.noreply.github.com>
2025-03-31 08:28:58 +0200

    updated website to include command documentation and proper footer

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,33 @@
-console.log("js is live")
+//basic commands
+
+document.getElementById("basicToggleCommands").addEventListener("change", function() {
+    let prefixCommands = document.querySelectorAll(".basicPrefix");
+    let slashCommands = document.querySelectorAll(".basicSlash");
+    let label = document.getElementById("basicToggleLabel");
+
+    if (this.checked) {
+        prefixCommands.forEach(cmd => cmd.classList.add("hidden"));
+        slashCommands.forEach(cmd => cmd.classList.remove("hidden"));
+        label.innerText = "Slash Commands";
+    } else {
+        prefixCommands.forEach(cmd => cmd.classList.remove("hidden"));
+        slashCommands.forEach(cmd => cmd.classList.add("hidden"));
+        label.innerText = "Prefix";
+    }
+});
+
+document.getElementById("moderationToggleCommands").addEventListener("change", function() {
+    let prefixCommands = document.querySelectorAll(".moderationPrefix");
+    let slashCommands = document.querySelectorAll(".moderationSlash");
+    let label = document.getElementById("moderationToggleLabel");
+
+    if (this.checked) {
+        prefixCommands.forEach(cmd => cmd.classList.add("hidden"));
+        slashCommands.forEach(cmd => cmd.classList.remove("hidden"));
+        label.innerText = "Slash Commands";
+    } else {
+        prefixCommands.forEach(cmd => cmd.classList.remove("hidden"));
+        slashCommands.forEach(cmd => cmd.classList.add("hidden"));
+        label.innerText = "Prefix";
+    }
+});
